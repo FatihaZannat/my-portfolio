c9df0779adb4b22ef3612faf5ba7d96d508e7c5a
Fatiha Zannat <fatihazannat.n@gmail.com>
2021-05-01 14:42:02 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+ 
+    <h3 className='primary'>this is portfolio</h3>
+  );
+}
+
+export default App;
